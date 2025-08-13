1950bac7fdf62226def8e3d5aac479e7ef6fdbc8
rdwivedi <rajdwivedipc@gmail.com>
2025-08-13 16:24:08 +0530

    created get user bank accounts and upi ids route

---
+++ src/controllers/user/accounts/getUserAccountsAndUPI.ts
@@ -0,0 +1,51 @@
+import prisma from "@/lib/prisma.js";
+import {Request, Response, NextFunction} from "express";
+import {z} from "zod";
+
+const schema = z.object({
+  user_id: z.string().min(1),
+}).required();
+
+export default async function getUserAccountsAndUPI(req: Request, res: Response, next: NextFunction) {
+
+  const {error, data: params} = schema.safeParse(req.params);
+  if (error) {
+    req.logger.info("Validation Error");
+    next(error); return;
+  }
+
+
+  try {
+
+    const data = await prisma.user.findUnique({
+      where: {
+        id: params.user_id
+      },
+      select: {
+        id: true,
+        user_upi_ids: true,
+        user_bank_accounts: true
+      }
+    });
+
+
+    if (!data) {
+      req.logger.info("user data not found")
+      res.status(404).send({
+        message: "Not Found",
+      })
+      return;
+    }
+
+    req.logger.info(data);
+
+    res.status(200).json(data);
+
+  } catch (e) {
+    console.error("Failed to find user's bank account and UPI Handles",e);
+    req.logger.error(e);
+    next(e);
+    return;
+  }
+
+}
--- src/router/accountRouter.ts
+++ src/router/accountRouter.ts
@@ -1,5 +1,6 @@
 import addBankAccount from "@/controllers/user/accounts/addBankAccount.js";
 import addUpiHandle from "@/controllers/user/accounts/addUpiHandle.js";
+import getUserAccountsAndUPI from "@/controllers/user/accounts/getUserAccountsAndUPI.js";
 import {Router} from "express";
 
 
@@ -9,6 +10,9 @@ const accountRouter = Router({mergeParams: true, caseSensitive: false});
 accountRouter.post("/bank-account", addBankAccount);
 
 accountRouter.post("/upi", addUpiHandle);
+
+accountRouter.get("/", getUserAccountsAndUPI)
+
 
 
 export default accountRouter;
